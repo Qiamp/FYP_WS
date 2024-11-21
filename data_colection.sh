514532b804834d75e03c795be225f61782c969e4
Qiamp <jayzhao0115@163.com>
2024-11-21 18:12:18 +0800

    Update calibrate.sh and data_colection.sh mavcmd to use the new camera calibration files

--- data_colection.sh
+++ data_colection.sh
@@ -9,6 +9,7 @@ echo "========== 2. Starting mavros =========="
 #source /home/nvidia/ws_uav_setup/devel/setup.bash
 sudo chmod 777 /dev/tty*
 roslaunch mavros px4.launch & sleep 5;
+rosrun mavros mavcmd long 511 31 10000 0 0 0 0 0
 
 echo "========== 3. Starting USB_CAM =========="
 roslaunch usb_cam usb_cam-test.launch & sleep 5;
