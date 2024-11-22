f06a78110c9f9a0e709d28fc64e4d971f6291acc
Qiamp <jayzhao0115@163.com>
2024-11-22 13:26:26 +0800

    Update calibrate.sh

--- calibrate.sh
+++ calibrate.sh
@@ -7,7 +7,7 @@ echo "========== 1. Starting mavros =========="
 #source /home/nvidia/ws_uav_setup/devel/setup.bash
 sudo chmod 777 /dev/tty*
 roslaunch mavros px4.launch & sleep 5;
-rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0
+# rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0
 
 echo "========== 2. Starting USB_CAM =========="
 roslaunch usb_cam usb_cam-test.launch & sleep 5;
