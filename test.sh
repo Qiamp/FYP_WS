d8743596bbaa61231894f3c013cd19c64e8c20ed
Qiamp <jayzhao0115@163.com>
2025-03-07 17:39:27 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /home/jay/FYP_WS/devel/setup.bash
-
-roslaunch image_proc apriltag.launch & sleep 1;
-roslaunch apriltag_ros continuous_detection.launch & sleep 1;
-roslaunch apriltag_pose_pub apriltag_tracking.launch
-
