081d573f618fbda2c7b3aae7e7a97419e9af09a0
Qiamp <jayzhao0115@163.com>
2025-03-08 14:24:28 +0800

    Update offboard_landing_gnss package

--- src/offboard_landing_gnss/src/offboard_landing_gnss.cpp
+++ src/offboard_landing_gnss/src/offboard_landing_gnss.cpp
@@ -1,4 +1,3 @@
-//包含ROS和MAVROS相关头文件 
 #include <string> 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -12,9 +11,7 @@
 #include <mavros_msgs/PositionTarget.h>
 #include <cmath>
 #include <tf/transform_listener.h>
-#include <nav_msgs/Odometry.h>
 #include <mavros_msgs/CommandLong.h>   
-#include <string>
 #include <geometry_msgs/Twist.h>
 
 #define ALTITUDE  1.5
@@ -31,33 +28,32 @@ float init_position_x_take_off =0;
 float init_position_y_take_off =0;
 float init_position_z_take_off =0;
 bool  flag_init_position = false;
-nav_msgs::Odometry local_pos;
-void local_pos_cb(const nav_msgs::Odometry::ConstPtr& msg);
+geometry_msgs::PoseStamped local_pos;
 
 void state_cb(const mavros_msgs::State::ConstPtr& msg)
 {
     current_state = *msg;
 }
 
-//回调函数接收无人机的里程计信息
-void local_pos_cb(const nav_msgs::Odometry::ConstPtr& msg)
+//回调函数接收无人机的初始位置信息
+void local_pos_cb(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
     local_pos = *msg;
-    if (flag_init_position==false && (local_pos.pose.pose.position.z!=0))
+    if (flag_init_position==false && (local_pos.pose.position.z !=0))
     {
-		init_position_x_take_off = local_pos.pose.pose.position.x;
-	    init_position_y_take_off = local_pos.pose.pose.position.y;
-	    init_position_z_take_off = local_pos.pose.pose.position.z;
-        flag_init_position = true;		    
+        init_position_x_take_off = local_pos.pose.position.x;
+        init_position_y_take_off = local_pos.pose.position.y;
+        init_position_z_take_off = local_pos.pose.position.z;
+        flag_init_position = true;        
     }
-    tf::quaternionMsgToTF(local_pos.pose.pose.orientation, quat);	
-	tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+    tf::quaternionMsgToTF(local_pos.pose.orientation, quat);
+    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
 }
 
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "offboard_gnss_landing");
+    ros::init(argc, argv, "offboard_landing_gnss");
 
     ros::NodeHandle nh;
 
@@ -65,7 +61,8 @@ int main(int argc, char **argv)
 
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
     
-    ros::Subscriber local_pos_sub = nh.subscribe<nav_msgs::Odometry>("/mavros/local_position/odom", 10, local_pos_cb);
+    ros::Subscriber local_pos_sub = nh.subscribe<geometry_msgs::PoseStamped>(
+        "/mavros/local_position/pose", 10, local_pos_cb);
 
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
 
@@ -82,10 +79,10 @@ int main(int argc, char **argv)
     }
 
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x =init_position_x_take_off + 0;
-    pose.pose.position.y =init_position_y_take_off + 0;
-    pose.pose.position.z =init_position_z_take_off + ALTITUDE;
-     
+    pose.pose.position.x = init_position_x_take_off + 0;
+    pose.pose.position.y = init_position_y_take_off + 0;
+    pose.pose.position.z = init_position_z_take_off + ALTITUDE;
+
 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i)
@@ -102,113 +99,112 @@ int main(int argc, char **argv)
     arm_cmd.request.value = true;
 
     ros::Time last_request = ros::Time::now();
- 
- 	//此处满足一次请求进入offboard模式即可，官方例成循环切入offboard会导致无人机无法使用遥控器控制
+
+ 	//此处满足一次请求进入offboard模式即可，官方例成循环切入offboard会导致无人机无法使用遥控器控制 
     while(ros::ok())
     {
-    	//请求进入OFFBOARD模式
-        if( current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(5.0)))
+        //请求进入OFFBOARD模式
+        if(current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(5.0)))
         {
-            if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent)
+            if(set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent)
             {
                 ROS_INFO("Offboard enabled");
             }
-           	last_request = ros::Time::now();
-       	}
+            last_request = ros::Time::now();
+        }
         else 
-		{
-			//请求解锁
-			if( !current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0)))
-			{
-		        if( arming_client.call(arm_cmd) && arm_cmd.response.success)
-		       	{
-		            ROS_INFO("Vehicle armed");
-		        }
-		        	last_request = ros::Time::now();
-			}
-		}
-	    
-	    if(fabs(local_pos.pose.pose.position.z- init_position_z_take_off -ALTITUDE)<0.2)
-		{	
-			if(ros::Time::now() - last_request > ros::Duration(3.0))
-			{
-				break;
-			}
-		}
+        {
+            //请求解锁
+            if(!current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0)))
+            {
+                if(arming_client.call(arm_cmd) && arm_cmd.response.success)
+                {
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+        
+        if(fabs(local_pos.pose.position.z - init_position_z_take_off - ALTITUDE) < 0.2) 
+        {    
+            if(ros::Time::now() - last_request > ros::Duration(3.0))
+            {
+                break;
+            }
+        }
 		//发布期望位置信息
-		pose.pose.position.x =init_position_x_take_off + 0;
-		pose.pose.position.y =init_position_y_take_off + 0;
-		pose.pose.position.z =init_position_z_take_off + ALTITUDE;
-		local_pos_pub.publish(pose);
+        pose.pose.position.x = init_position_x_take_off + 0;
+        pose.pose.position.y = init_position_y_take_off + 0;
+        pose.pose.position.z = init_position_z_take_off + ALTITUDE;
+        local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
     }   
-    
+
     
     while(ros::ok())
     {
-        if((flag == 1)  && (ros::Time::now() - last_request > ros::Duration(5.0)))
-		{ 
-			ROS_INFO("Position_1");
-            pose.pose.position.x =init_position_x_take_off + 0;
-			pose.pose.position.y =init_position_y_take_off + 0;
-			pose.pose.position.z =init_position_z_take_off + ALTITUDE;                     
-			last_request = ros::Time::now();
+        if((flag == 1) && (ros::Time::now() - last_request > ros::Duration(5.0)))
+        { 
+            ROS_INFO("Position_1");
+            pose.pose.position.x = init_position_x_take_off + 0;
+            pose.pose.position.y = init_position_y_take_off + 0;
+            pose.pose.position.z = init_position_z_take_off + ALTITUDE;                     
+            last_request = ros::Time::now();
             flag=2;
         }
 
-		if((flag ==2) && (ros::Time::now() - last_request > ros::Duration(8.0)))
-		{
-			ROS_INFO("Position_2 ");
-		    pose.pose.position.x =init_position_x_take_off + 2;
-			pose.pose.position.y =init_position_y_take_off + 0;
-			pose.pose.position.z =init_position_z_take_off + ALTITUDE;     
-			last_request = ros::Time::now();
-			flag=3;
-		}
-		                   
-		if((flag ==3) && (ros::Time::now() - last_request > ros::Duration(8.0)))
-		{
-		    ROS_INFO("Position_3 ");
-		    pose.pose.position.x =init_position_x_take_off + 2;
-			pose.pose.position.y =init_position_y_take_off + 2;
-			pose.pose.position.z =init_position_z_take_off + ALTITUDE;      
-		    last_request = ros::Time::now();
-			flag=4;
-		}
-
-		if((flag ==4) && (ros::Time::now() - last_request > ros::Duration(8.0)))
-		{
-		    ROS_INFO("Position_4 ");
-		    pose.pose.position.x =init_position_x_take_off + 0;
-			pose.pose.position.y =init_position_y_take_off + 2;
-			pose.pose.position.z =init_position_z_take_off + ALTITUDE;     
-		    last_request = ros::Time::now();
-			flag=5;
-		}
-
-		if((flag ==5) && (ros::Time::now() - last_request > ros::Duration(8.0)))
-		{
-            ROS_INFO("Position_1 ");
-            pose.pose.position.x =init_position_x_take_off + 0;
-			pose.pose.position.y =init_position_y_take_off + 0;
-			pose.pose.position.z =init_position_z_take_off + ALTITUDE;     		
-            last_request = ros::Time::now();
-			flag=6;
-        }
-       if((flag ==6) && (ros::Time::now() - last_request > ros::Duration(8.0)))
+        if((flag == 2) && (ros::Time::now() - last_request > ros::Duration(8.0)))
         {
-			ROS_INFO("AUTO.LAND");
- 			pose.pose.position.x =init_position_x_take_off + 0;
-			pose.pose.position.y =init_position_y_take_off + 0;
-			pose.pose.position.z =init_position_z_take_off - 1;    
+            ROS_INFO("Position_2 ");
+            pose.pose.position.x = init_position_x_take_off + 2;
+            pose.pose.position.y = init_position_y_take_off + 0;
+            pose.pose.position.z = init_position_z_take_off + ALTITUDE;     
+            last_request = ros::Time::now();
+            flag=3;
         }
+                           
+        if((flag == 3) && (ros::Time::now() - last_request > ros::Duration(8.0)))
+        {
+            ROS_INFO("Position_3 ");
+            pose.pose.position.x = init_position_x_take_off + 2;
+            pose.pose.position.y = init_position_y_take_off + 2;
+            pose.pose.position.z = init_position_z_take_off + ALTITUDE;      
+            last_request = ros::Time::now();
+            flag=4;
+        }
+
+        if((flag == 4) && (ros::Time::now() - last_request > ros::Duration(8.0)))
+        {
+            ROS_INFO("Position_4 ");
+            pose.pose.position.x = init_position_x_take_off + 0;
+            pose.pose.position.y = init_position_y_take_off + 2;
+            pose.pose.position.z = init_position_z_take_off + ALTITUDE;     
+            last_request = ros::Time::now();
+            flag=5;
+        }
+
+        if((flag == 5) && (ros::Time::now() - last_request > ros::Duration(8.0)))
+        {
+            ROS_INFO("Position_1 ");
+            pose.pose.position.x = init_position_x_take_off + 0;
+            pose.pose.position.y = init_position_y_take_off + 0;
+            pose.pose.position.z = init_position_z_take_off + ALTITUDE;     		
+            last_request = ros::Time::now();
+            flag=6;
+        }
+        
+        if((flag == 6) && (ros::Time::now() - last_request > ros::Duration(8.0)))
+        {
+            ROS_INFO("AUTO.LAND");
+            pose.pose.position.x = init_position_x_take_off + 0;
+            pose.pose.position.y = init_position_y_take_off + 0;
+            pose.pose.position.z = init_position_z_take_off - 1;    
+        }
+        
         local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
     }
     return 0;
 }
-
-
-
